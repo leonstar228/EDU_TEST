835f22ec975600ecc5e6438bae12bfb900d08025
Alina <AlinaBlednova2007@gmail.com>
2026-02-17 18:59:06 +0200

    Add project files.

---
+++ EDU_TEST/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EDU_TEST.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
